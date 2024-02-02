5e9cab21a65d2a81adf801b8f5d2eff35a26aa1f
Nguyen Quang Dieu <86088186+itzmealvin@users.noreply.github.com>
2024-02-03 05:59:52 +0700

    Update DoubleTransfer.sol

--- contracts/DoubleTransfer.sol
+++ contracts/DoubleTransfer.sol
@@ -4,20 +4,21 @@ pragma solidity ^0.8;
 contract DoubleTransfer {
     // create additional stats for contracts
     address payable public owner;
-    uint256[2] public totalStats; // first is totalTxn, second is totalVolume
+    uint64 public fee = 500_000 gwei;
     uint256 private totalEscrowing;
-    uint64 public fee = 100_000 gwei;
+    uint256[2] public totalStats; // first is totalTxn, second is totalVolume
     Order[] public allOrders;
 
     // create enumurate Status
     enum Status {
-        NOT_INIT,
+        CANCELLED,
         CREATED,
-        SENT
+        COMPLETED
     }
 
     // define the Order structure
     struct Order {
+        uint256 orderId;
         uint256 timestamp;
         address receiver;
         uint256 amount;
@@ -48,19 +49,24 @@ contract DoubleTransfer {
         _;
     }
 
-    // prevent re-entrancy
-    modifier onlyHuman() {
-        uint size;
-        address addr = msg.sender;
-        assembly {
-            size := extcodesize(addr)
-        }
-        require(size == 0, "ONLY HUMAN PLEASE");
+    // check if only the sender of the order
+    modifier onlySender(uint256 _orderId) {
+        require(orderSender[_orderId] == msg.sender, "You are not the sender");
+        _;
+    }
+
+    // check if order is valid and returns it
+    modifier checkOrder(uint256 _orderId) {
+        require(
+            orderStatus[_orderId] != Status.COMPLETED ||
+                orderStatus[_orderId] != Status.CANCELLED,
+            "Order is either completed or cancelled"
+        );
         _;
     }
 
     // step 1: initialize an order
-    function createNewOrder(address _receiver) external payable onlyHuman {
+    function createNewOrder(address _receiver) external payable {
         require(
             msg.value > fee,
             "Must send enough Ether with the function call"
@@ -68,87 +74,99 @@ contract DoubleTransfer {
         (bool s, ) = address(this).call{value: msg.value}("");
         require(s, "Error in transfer");
         uint256 amount = msg.value - fee;
-        totalEscrowing += finalValue;
-        Order memory tempOrder = Order(block.timestamp, _receiver, amount);
-        orderStatus[allOrders.length] = Status.CREATED;
-        totalStats
-
-        userOrders[msg.sender][userCurrentNonce[msg.sender]++] = Order(
-            userCurrentNonce[msg.sender],
+        totalEscrowing += amount;
+        Order memory tempOrder = Order(
+            allOrders.length,
             block.timestamp,
-            msg.sender,
             _receiver,
-            finalValue,
-            Stages.INIT
+            amount
         );
-        userNonces[msg.sender].push(userCurrentNonce[msg.sender]);
+        allOrders.push(tempOrder);
+        orderStatus[tempOrder.orderId] = Status.CREATED;
+        orderSender[tempOrder.orderId] = msg.sender;
     }
 
     // step 2a: confirm this order, this cannot be undone
-    function confirmCurrentOrder(uint _nonce) external payable onlyHuman {
-        Order storage orderDetails = checkOrder(_nonce);
-        (bool s, ) = orderDetails.receiver.call{value: orderDetails.amount}("");
+    function confirmCurrentOrder(
+        uint _orderId
+    ) external payable onlySender(_orderId) checkOrder(_orderId) {
+        Order storage selectedOrder = allOrders[_orderId];
+        (bool s, ) = selectedOrder.receiver.call{value: selectedOrder.amount}(
+            ""
+        );
         require(s, "Error in transfer");
-        orderDetails.stage = Stages.SENT;
-        uint256 actualAmount = orderDetails.amount;
-        totalEscrowing -= actualAmount;
-        totalVolume += actualAmount; // this is the total volume of the protocol
+        orderStatus[_orderId] = Status.COMPLETED;
+        uint256 amount = selectedOrder.amount;
+        totalEscrowing -= amount;
+        totalStats[0]++;
+        totalStats[1] += amount;
         userStats[msg.sender][0]++; // this is the total txns of the sender
-        userStats[msg.sender][1] += actualAmount; // this is the the volume of sender
+        userStats[msg.sender][1] += amount; // this is the the volume of sender
     }
 
     // step 2b: modify receiver in case of mistakes
     function modifyCurrentReceiver(
-        uint _nonce,
+        uint _orderId,
         address _newReceiver
-    ) external onlyHuman {
-        Order storage orderDetails = checkOrder(_nonce);
-        orderDetails.receiver = _newReceiver;
+    ) external onlySender(_orderId) checkOrder(_orderId) {
+        Order storage selectedOrder = allOrders[_orderId];
+        selectedOrder.receiver = _newReceiver;
     }
 
     // step 2c: cancel order if no longer want to transfer
-    function cancelCurrentOrder(uint _nonce) external onlyHuman {
-        Order storage orderDetails = checkOrder(_nonce);
-        (bool s, ) = orderDetails.sender.call{value: orderDetails.amount}("");
+    function cancelCurrentOrder(
+        uint _orderId
+    ) external onlySender(_orderId) checkOrder(_orderId) {
+        Order storage selectedOrder = allOrders[_orderId];
+        (bool s, ) = msg.sender.call{value: selectedOrder.amount}("");
         require(s, "Error in transfer");
-        orderDetails.stage = Stages.CANCELLED;
-        totalEscrowing -= orderDetails.amount;
+        orderStatus[_orderId] = Status.CANCELLED;
+        totalEscrowing -= selectedOrder.amount;
     }
 
     // view specific order
-    function viewOrder(
-        address _sender,
-        uint _nonce
-    ) external view returns (Order memory) {
-        return userOrders[_sender][_nonce];
+    function viewOrder(uint _orderId) external view returns (Order memory) {
+        return allOrders[_orderId];
     }
 
-    // check if order is valid and returns it
-    modifier checkOrder(uint256 _nonce) {
-        require(isSent[_nonce], "Order is completed");
-        _;
+    // view all orders of an address
+    function viewAllOrdersOf() external view returns (Order[] memory) {
+        Order[] memory results = new Order[](userStats[msg.sender][1]);
+        uint counter = 0;
+        for (uint i = 0; i < allOrders.length; i++) {
+            if (orderSender[i] == msg.sender) {
+                results[counter] = allOrders[i];
+                counter++;
+            }
+        }
+        return results;
     }
 
     // view current sender nonces
-    function viewCurrentNonces(
-        address _sender
-    ) external view returns (uint[] memory) {
-        return userNonces[_sender];
+    function viewUserCurrentStats() external view returns (uint[2] memory) {
+        return userStats[msg.sender];
+    }
+
+    // view current sender nonces
+    function viewTotalCurrentStats() external view returns (uint[2] memory) {
+        return totalStats;
     }
 
     // FOR OWNER: view available fee to withdraw
     function viewAvailableFee() public view returns (uint256) {
-        return address(this).balance - totalEscrowing;
+        return address(this).balance - totalEscrowing - fee;
     }
 
     // FOR OWNER: set new fee to charge
     function setNewFee(uint64 _newFee) external onlyOwner {
+        require(_newFee >= fee);
         fee = _newFee;
     }
 
     // FOR OWNER: withdraw available fee
     function withdrawFee() external payable onlyOwner {
         uint256 availableFee = viewAvailableFee();
+        require(availableFee > 0, "Not enough fee to withdraw");
         (bool s, ) = owner.call{value: availableFee}("");
         require(s);
     }
@@ -160,7 +178,7 @@ contract DoubleTransfer {
     }
 
     // FOR OWNER: self-destruct the contract
-    function close() public onlyOwner {
+    function close() external onlyOwner {
         require(totalEscrowing == 0, "There is still users' escrowed here");
         selfdestruct(owner);
     }
